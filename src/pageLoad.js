c71f1de1070f663bb61a1972594ae79ad1745f75
MrDomoArigato98 <domdob98@gmail.com>
2025-03-02 11:23:55 +0000

    Functional website, didn't make more pages

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,33 +1,57 @@
 import chickenImage from "./img/fried-chicken-breast-with-vegetables.jpg"
+import potWithChickemImage from "./img/3253.jpg"
+import starterImage from "./img/3418.jpg"
+import beefImage from "./img/14307.jpg"
 
 function loadHomePageFn(){
     //Get ID of content
     const content = document.getElementById("content")
 
-    //Creating a div with class "card"
     const cardDiv = document.createElement("div")
+    const bannerDiv = document.createElement("div")
+    bannerDiv.classList.add("banner")
     cardDiv.classList.add("card");  
 
-    //Creates a header with text, appends to cardDiv
-    const cardHeader = document.createElement("h2")
-    cardHeader.textContent = "Look at how amazing this restaurant is!"
-    cardDiv.appendChild(cardHeader);
 
     //Creates cardImage, imports from files, appends to cardImage
     const cardImage = document.createElement("img")
     cardImage.src = chickenImage;
     cardDiv.appendChild(cardImage)
-
     //Appends to DOM
-    content.appendChild(cardDiv)
+
+    
 
     const descriptionDiv = document.createElement("div")
     descriptionDiv.classList.add("card","description")
     const descriptionP = document.createElement("p")
-    descriptionP.textContent="This fabulous meal will be the cheapest and most delicious meal in the universe."
+    descriptionP.textContent="Welcome to The Culinary Corner, where every dish is a celebration of flavor and creativity. Join us and discover a taste of culinary excellence that will leave you craving more."
     descriptionDiv.appendChild(descriptionP)
 
-    content.appendChild(descriptionDiv)
+    bannerDiv.appendChild(cardDiv)
+    bannerDiv.appendChild(descriptionDiv)
+    content.appendChild(bannerDiv)
+
+    const dishesText = document.createElement("p")
+    dishesText.textContent = "Check out our full Menu."
+    const dishesTextDiv = document.createElement("div")
+    dishesTextDiv.appendChild(dishesText)
+
+    dishesTextDiv.classList.add("dishes-description")
+    content.appendChild(dishesTextDiv)
+
+    const dishOneImage = document.createElement("img")
+    dishOneImage.src=potWithChickemImage;
+    const dishTwoImage = document.createElement("img")
+    dishTwoImage.src = starterImage
+    const dishThreeImage = document.createElement("img")
+    dishThreeImage.src = beefImage
+    const dishes = document.createElement("div")
+    dishes.classList.add("dishes")
+    dishes.appendChild(dishOneImage)
+    dishes.appendChild(dishTwoImage)
+    dishes.appendChild(dishThreeImage)
+
+    content.appendChild(dishes)
 }
 
 
